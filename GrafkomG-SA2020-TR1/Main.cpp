4c48cb75f88a2f833b0ef00a6fbd87ee04336397
robertusbagaskara <robertusbagaskara@gmail.com>
2020-07-19 20:57:31 +0700

    add building J

--- GrafkomG-SA2020-TR1/Main.cpp
+++ GrafkomG-SA2020-TR1/Main.cpp
@@ -2505,8 +2505,8 @@ void drawBuildingI() {
 	glBegin(GL_POLYGON);
 	glVertex3f(-25.0, 55.0, -750.0);
 	glVertex3f(-500.0, 55.0, -750.0);
-	glVertex3f(-500.0, 55.0, -400.0);
-	glVertex3f(-25.0, 55.0, -400.0);
+	glVertex3f(-500.0, 55.0, -600.0);
+	glVertex3f(-25.0, 55.0, -600.0);
 	glEnd();
 
 	//depan
@@ -2548,6 +2548,60 @@ void drawBuildingI() {
 	glVertex3f(-500.0, 350.0, -750.0);
 	glVertex3f(-500.0, 350.0, -400.0);
 	glVertex3f(-25.0, 350.0, -400.0);
+	glEnd();
+}
+
+void drawBuildingJ() {
+	myColor color;
+	glColor3fv(color.glass);
+
+	//bawah
+	glBegin(GL_POLYGON);
+	glVertex3f(200.0, 55.0, -750.0);
+	glVertex3f(20.0, 55.0, -750.0);
+	glVertex3f(20.0, 55.0, -600.0);
+	glVertex3f(200.0, 55.0, -600.0);
+	glEnd();
+
+	//depan
+	glBegin(GL_POLYGON);
+	glVertex3f(20.0, 55.0, -600.0);
+	glVertex3f(200.0, 55.0, -600.0);
+	glVertex3f(200.0, 400.0, -600.0);
+	glVertex3f(20.0, 400.0, -600.0);
+	glEnd();
+
+	//belakang
+	glBegin(GL_POLYGON);
+	glVertex3f(200.0, 55.0, -750.0);
+	glVertex3f(20.0, 55.0, -750.0);
+	glVertex3f(20.0, 400.0, -750.0);
+	glVertex3f(200.0, 400.0, -750.0);
+	glEnd();
+
+	//kanan
+	glBegin(GL_POLYGON);
+	glVertex3f(200.0, 55.0, -750.0);
+	glVertex3f(200.0, 55.0, -600.0);
+	glVertex3f(200.0, 400.0, -600.0);
+	glVertex3f(200.0, 400.0, -750.0);
+	glEnd();
+
+	//kiri
+	glBegin(GL_POLYGON);
+	glVertex3f(20.0, 55.0, -750.0);
+	glVertex3f(20.0, 55.0, -600.0);
+	glVertex3f(20.0, 400.0, -600.0);
+	glVertex3f(20.0, 400.0, -750.0);
+	glEnd();
+
+	glColor3fv(color.white);
+	//atas
+	glBegin(GL_POLYGON);
+	glVertex3f(200.0, 400.0, -750.0);
+	glVertex3f(20.0, 400.0, -750.0);
+	glVertex3f(20.0, 400.0, -600.0);
+	glVertex3f(200.0, 400.0, -600.0);
 	glEnd();
 }
 
@@ -2764,6 +2818,7 @@ void display() {
 
 	drawBuildingH();
 	drawBuildingI();
+	drawBuildingJ();
 
 	myTree(170.0, 340);
 	myTree(170.0, 130);
